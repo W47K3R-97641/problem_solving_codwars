7f4d8a5a8134dba4b16b6bb69b62a5633504fc18
The Shadow <74643315+amarochadli92@users.noreply.github.com>
2022-11-03 20:59:45 +0100

    solving

--- You-only-need-one.js
+++ You-only-need-one.js
@@ -2,5 +2,3 @@ function check(a, x) {
     // your code
     return a.indexOf(x) >= 0 ? true : false;
 }
-
-console.log(check([101, 45, 75, 105, 99, 107], 107));
---
+++ stringToNumber.js
@@ -0,0 +1,6 @@
+const stringToNumber = function(str) {
+    // put your code here
+    return parseInt(str);
+};
+
+console.log(parseInt("-100"));
